6cdec76f93fd3e1349d1ba5e4982524669ae7fc2
htq-uet <teoemga2003@gmail.com>
2023-09-06 13:03:10 +0700

    First Initialize

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NumberPicker numberPicker;
+
+    private EditText editAmount;
+
+    private Button donateButton;
+
+    private TextView amountTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Hide the soft keyboard when the user taps outside an EditText
+        View mainLayout = findViewById(R.id.relativeLayout);
+        mainLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    View focusedView = getCurrentFocus();
+                    if (focusedView != null) {
+                        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(focusedView.getWindowToken(), 0);
+                    }
+                }
+                return false;
+            }
+        });
+
+        // NumberPicker setup
+
+        numberPicker = findViewById(R.id.numberPicker);
+        numberPicker.setEnabled(true);
+        numberPicker.setFocusable(false);
+        numberPicker.setClickable(false);
+        numberPicker.setFocusableInTouchMode(false);
+        numberPicker.setWrapSelectorWheel(true);
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(1000);
+        numberPicker.setValue(999);
+        numberPicker.setOnValueChangedListener((picker, oldVal, newVal) -> {
+
+        });
+
+        editAmount = findViewById(R.id.editTextText);
+        donateButton = findViewById(R.id.button);
+        amountTextView = findViewById(R.id.textView2);
+
+
+
+        AtomicReference<Integer> sum = new AtomicReference<>(0);
+
+        //Update the amount donated when the user enters a new value
+        editAmount.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // Get the entered amount from the EditText
+                String amountStr = editAmount.getText().toString().trim();
+
+                if (!amountStr.isEmpty()) {
+                    int amount = Integer.parseInt(amountStr);
+
+                    // Update the sum and display it
+                    sum.updateAndGet(currentSum -> currentSum + amount);
+                    amountTextView.setText("Total so Far: $" + sum.get());
+
+                    // Clear the EditText
+                    editAmount.setText("");
+
+                    // Close the soft keyboard
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                } else {
+                    // Handle the case where the EditText is empty or contains invalid input
+                    // You can show an error message or take appropriate action here
+                }
+
+                return true; // Consume the event
+            }
+            return false;
+        });
+
+        //Update the amount donated when the user clicks the donate button
+        donateButton.setOnClickListener(v -> {
+            Integer amount = editAmount.getText().toString().isEmpty() ? 0 : Integer.parseInt(editAmount.getText().toString());
+            sum.updateAndGet(v1 -> v1 + amount);
+            amountTextView.setText("Total so Far  $" + sum);
+        });
+    }
+}
