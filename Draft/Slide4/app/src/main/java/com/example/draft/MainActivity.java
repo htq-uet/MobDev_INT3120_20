f982eeb3dcc4da0e5af88260ebca001f748821b6
htq-uet <teoemga2003@gmail.com>
2023-09-20 23:15:27 +0700

    Slide 6 draft

---
+++ Draft/Slide4/app/src/main/java/com/example/draft/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.draft;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+// Define the MainActivity class and implement the AdapterView.OnItemSelectedListener interface
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+
+    // Declare a TextView to display the selected item
+    TextView selection;
+
+    // Define an array of items for the Spinner
+    String[] items = {"Android", "IPhone", "WindowsMobile",
+            "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X"};
+
+    // onCreate method, called when the activity is created
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        // Set the layout for this activity based on the XML layout file
+        setContentView(R.layout.activity_main);
+
+        // Initialize the 'selection' TextView by finding it in the layout XML with its ID
+        selection = (TextView) findViewById(R.id.selection);
+
+        // Set the default selection of the 'selection' TextView
+        selection.setText(items[0]);
+
+        // Initialize the Spinner by finding it in the layout XML with its ID
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+
+        // Set an OnItemSelectedListener for the Spinner
+        spin.setOnItemSelectedListener(this);
+
+        // Create an ArrayAdapter to populate the Spinner with items
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                items);
+
+        // Set the dropdown view resource for the ArrayAdapter to use a simple dropdown layout
+        aa.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter as the adapter for the Spinner
+        spin.setAdapter(aa);
+        
+        View mainLayout = findViewById(R.id.myLinearLayout);
+
+        // Set the OnClickListener for the main layout
+        mainLayout.setOnClickListener(this);
+
+        // Unrepresented the Spinner when the main layout is clicked
+        mainLayout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //set null selection
+                spin.setSelection(0);
+            }
+        });
+
+
+    }
+
+    // onItemSelected method, called when an item in the Spinner is selected
+    public void onItemSelected(AdapterView<?> parent,
+                               View v, int position, long id) {
+        // Update the 'selection' TextView with the selected item from the Spinner
+        selection.setText(items[position]);
+    }
+
+    // onNothingSelected method, called when no item is selected in the Spinner
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Clear the text in the 'selection' TextView
+        selection.setText(items[0]);
+    }
+
+    // onClick method, called when the main layout is clicked
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        selection.setText(items[0]);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+}
---
+++ Draft/Slide6/app/src/main/java/com/example/slide6/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.slide6;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Button popupButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        this.button = (Button) findViewById(R.id.button);
+        registerForContextMenu(button);
+
+        this.popupButton = (Button) findViewById(R.id.button_popup);
+        popupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu();
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.settings) {
+            Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.about) {
+            Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onContextItemSelected(item);
+    }
+
+    private void showPopupMenu() {
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, this.popupButton);
+        popupMenu.inflate(R.menu.menu_popup);
+        Menu menu = popupMenu.getMenu();
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        popupMenu.show();
+    }
+}
